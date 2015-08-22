5fe4740b775174623f3fbf741ac674663235f22f
Adam Goth <adamgoth@gmail.com>
2015-08-21 22:25:44 -0500

    Minor update to contest entry show view

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,5 +4,5 @@ class Entry < ActiveRecord::Base
 	belongs_to :game
 	belongs_to :selected_winner, :class_name => 'Team', :foreign_key => 'selected_winner_id'
 
-	validates :user_id, :contest_id, :game_id, :selected_winner, :weight, presence: true
+	validates :user_id, :contest_id, :game_id, :selected_winner_id, :weight, presence: true
 end
